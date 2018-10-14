b9aff25c808d52e73554a62cf39e91dfe8019e04
Roman M <rmakarov@accusoft.com>
2018-10-14 11:53:36 +0700

    impl. loading of products and css

--- chem.js
+++ chem.js
@@ -1,23 +1,57 @@
 //alert("hello");
 
+/*
 var categories = [
 	"Галогены",//0
-	"O2",//1
+	"O<sub>2</sub>",//1
 	"Cr"]//2
 
 var reactions = { raw : [
-	"0. KMnO4 + KNO2 + H2O -> MnO4 + KNO3 + KOH - K2MnO4 - O2 - H2 - H20"//0
+	"0. KMnO4 + KNO2 + H2O -> MnO4+KNO3+KOH-K2MnO4-O2-H2-H2O"//0
+]}
+*/
+var categories = [
+	"Галогены",//0
+	"Сера",//1
+	"Азот",//2
+	"Фосфор",//3
+	"Углерод",//4
+	"Кремний"]//5
+
+var reactions = { raw : [
+	"2. KMnO4 + KNO2 + H2O -> MnO2+KNO3+KOH-K2MnO4-O2-H2-H2O",//0
+	"1. SO2 + KMnO4 + H2O -> MnSO4 + K2SO4 + H2SO4 - SO3 - S - H2S - MnO2 - K2MnO4 - KOH - H2O - O2",//1
+	"1. SO2 + KMnO4 + KOH -> K2MnO4 +K2SO4 + H2O - H2SO4 - SO3 - S - H2S - MnO2 - O2",//2
+	"3. PH3 + KMnO4 + KOH -> K3PO4 + K2MnO4 + H2O - H3PO4 - P4 - P2O5 - MnO2 - O2"//3
 ]}
 
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
 function parseReactions() {
+	var productRegex = /[\+\-]\s*[^\s\+\-]+/gi;
+	var digitRegex = /([a-z])(\d+)/gi;
 	reactions.parsed = [];
 	for (r in reactions.raw) {
 		var obj = {};
-		var s = reactions.raw[r];
+		var s = reactions.raw[r].replace(digitRegex, "$1<sub>$2</sub>");
 		var i1 = s.indexOf(".");
 		var i2 = s.indexOf("->");
 		obj.categoryId = s.slice(0, i1);
 		obj.reagents = s.slice(i1+1, i2).trim();
+		obj.products = shuffle(("+"+s.slice(i2+2).trim()).match(productRegex));	
 		reactions.parsed.push(obj);
 	}
 }
@@ -29,6 +63,7 @@ function cloneElement(elClassName, arrStrings, initFunc) {
 		var cloned = el.clone(false);
 		if (initFunc(cloned, str)) {
 			last.after(cloned);
+			cloned.show();
 			last = cloned;
 		}
 	}
@@ -37,17 +72,32 @@ function cloneElement(elClassName, arrStrings, initFunc) {
 function switchCategory(catId) {
 	//alert(id);	
 	$(".reaction:visible").remove();
+	$(".product:visible").remove();
+	$("#reactionPane").html("");
 	cloneElement("reaction", reactions.parsed, function(el, reactId) {
 		var reaction = reactions.parsed[reactId];
 		if (~catId && reaction.categoryId != catId) return false;
 		el.html(reaction.reagents);
-		el.show();
+		el.click(function() {
+			switchReaction(reactId);
+		})
+		return true;
+	});
+}
+
+function switchReaction(reactId) {
+	var reaction = reactions.parsed[reactId];
+	$(".product:visible").remove();
+	$("#reactionPane").html(reaction.reagents + " -> ");
+	cloneElement("product", reaction.products, function(el, prodId) {
+		el.html(reaction.products[prodId]);
 		return true;
 	});
 }
 
 // Shorthand for $( document ).ready()
 $(function() {
+	$(".reaction, .product").hide();
 	parseReactions();
 	$(".category").click(function() {
 		switchCategory(-1);
